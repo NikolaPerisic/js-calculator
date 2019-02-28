4d305ee83b88bccb116f2db4be267ebcfcf0d147
Nikola Perisic <cathode.y2k@gmail.com>
2019-02-28 14:55:38 +0100

    added clearHandler and deleteHandler function

--- src/Containers/Calculator.js
+++ src/Containers/Calculator.js
@@ -2,38 +2,19 @@ import React, { Component } from "react";
 import "../App.scss";
 import Controls from "../Components/Controls";
 import Display from "../Components/Display";
+import Data from "../assets/Data/Data";
 
 class Calculator extends Component {
-    state = {
-        calc: {
-            expression: "0",
-            output: "0",
-            decimalActive: false
-        },
-        clear: "C",
-        mod: "%",
-        divide: "/",
-        multiply: "*",
-        seven: "7",
-        eight: "8",
-        nine: "9",
-        subtract: "-",
-        four: "4",
-        five: "5",
-        six: "6",
-        add: "+",
-        one: "1",
-        two: "2",
-        three: "3",
-        decimal: ".",
-        zero: "0",
-        delete: "DEL",
-        equals: "="
-    };
+    state = { ...Data };
+
     inputHandler = el => {
         const item = el.currentTarget.textContent;
         const data = { ...this.state.calc };
         const lastChar = data.expression.charAt(data.expression.length - 1);
+
+        if (item === "DEL") return this.deleteHandler(lastChar);
+        if (item === "C") return this.clearAllHandler();
+        if (item === "=") return this.expressionHandler(data.expression);
 
         if (data.expression === "0") {
             if (!isNaN(parseInt(item))) {
@@ -61,6 +42,28 @@ class Calculator extends Component {
         }
         this.setState({ calc: data });
         return null;
+    };
+    deleteHandler = lastChar => {
+        const data = { ...this.state.calc };
+        if (data.output.length > 1 && !isNaN(parseInt(lastChar))) {
+            data.output = data.output.substring(0, data.output.length - 1);
+            data.expression = data.expression.substring(
+                0,
+                data.expression.length - 1
+            );
+        } else if (
+            (data.output.length > 1 && isNaN(parseInt(lastChar))) ||
+            data.output.length === 1
+        ) {
+            return this.clearAllHandler();
+        }
+        return this.setState({ calc: data });
+    };
+    clearAllHandler = () => {
+        return this.setState({ calc: Data.calc });
+    };
+    expressionHandler = str => {
+        console.log(str);
     };
     render() {
         const controls = Object.entries(this.state).map(([key, val]) => {
