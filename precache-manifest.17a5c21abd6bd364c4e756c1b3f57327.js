9d29d60064eb21585d74c44d82e605e548b5a9f8
Nikola Perisic <cathode.y2k@gmail.com>
2019-03-02 20:32:09 +0100

    Updates

---
+++ precache-manifest.17a5c21abd6bd364c4e756c1b3f57327.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61779590c70f474bd504",
+    "url": "/js-calculator/static/js/runtime~main.61779590.js"
+  },
+  {
+    "revision": "6fc6dcaac17c8743a410",
+    "url": "/js-calculator/static/js/main.6fc6dcaa.chunk.js"
+  },
+  {
+    "revision": "7c12d386b5bdf8580fdc",
+    "url": "/js-calculator/static/js/2.7c12d386.chunk.js"
+  },
+  {
+    "revision": "6fc6dcaac17c8743a410",
+    "url": "/js-calculator/static/css/main.93ed6427.chunk.css"
+  },
+  {
+    "revision": "7eeacab38d5345f0627c90eadcb6dfaa",
+    "url": "/js-calculator/index.html"
+  }
+];
